cb7e5675090be5b3684e4ae14feb7c3c71e0c154
Ilya <bocharov96@bk.ru>
2019-09-09 18:57:39 +0300

    url3

--- Leus/js/common.js
+++ Leus/js/common.js
@@ -240,7 +240,7 @@ $(function() {
 
 		var url = $(location).attr('pathname');
 		var width = $(window).width();
-		if( url == "/Leus/index.html" || url == "Leus/" && width > 768) {
+		if( url == "Leus/index.html" || url == "Leus/" && width > 768) {
 
 			$('.hamburger').on('click', toggleMenu);
 			setTimeout(function(){
@@ -250,7 +250,7 @@ $(function() {
 				arrowRight();
 			}, 1600);
 
-		} else if ( url == "/Leus/index.html" || url == "Leus/" && width < 768 ) {
+		} else if ( url == "Leus/index.html" || url == "Leus/" && width < 768 ) {
 			$('.hamburger').on('click', toggleMenuSmall);
 		}
 
