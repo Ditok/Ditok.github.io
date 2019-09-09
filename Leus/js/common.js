c689fd809a7d193b80695c8a9dac48ae4c9d063f
Ilya <bocharov96@bk.ru>
2019-09-09 18:55:43 +0300

    url2

--- Leus/js/common.js
+++ Leus/js/common.js
@@ -240,7 +240,7 @@ $(function() {
 
 		var url = $(location).attr('pathname');
 		var width = $(window).width();
-		if( url == "https://ditok.github.io/Leus/index.html" || url == "https://ditok.github.io/Leus/" && width > 768) {
+		if( url == "/Leus/index.html" || url == "Leus/" && width > 768) {
 
 			$('.hamburger').on('click', toggleMenu);
 			setTimeout(function(){
@@ -250,7 +250,7 @@ $(function() {
 				arrowRight();
 			}, 1600);
 
-		} else if ( url == "https://ditok.github.io/Leus/index.html" || url == "https://ditok.github.io/Leus/" && width < 768 ) {
+		} else if ( url == "/Leus/index.html" || url == "Leus/" && width < 768 ) {
 			$('.hamburger').on('click', toggleMenuSmall);
 		}
 
